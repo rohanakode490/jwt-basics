c8e2430331e9abe4cec3891794a793589061e2d6
Rohan <rohanakode52@gmail.com>
2023-06-27 23:20:15 +0530

    Authentication Middleware

--- controllers/main.js
+++ controllers/main.js
@@ -15,7 +15,7 @@ const login = async (req, res) => {
     }
 
     // id for demo, will be fetch from the database 
-    const id = new Date
+    const id = new Date().getDate()
 
     // new token - try to keep payload small, better experience for user
     const token = jwt.sign({id, username}, process.env.JWT_SECRET,{expiresIn:'30d'})
@@ -23,27 +23,13 @@ const login = async (req, res) => {
     res.status(200).json({msg:`user created`, token })
 };
 
-const dashboard = async (req, res) => { 
-    const authHeader = req.headers.authorization
-
-    if(!authHeader || !authHeader.startsWith('Bearer')){
-        throw new CustomAPIError("No Token provided", 401);
-    }
+const dashboard = async (req, res) => {     
+    const luckyNumber = Math.floor(Math.random() * 100);
     
-    const token = authHeader.split(' ')[1]
-    
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        
-        const luckyNumber = Math.floor(Math.random() * 100);
-        res.status(200).json({
-            msg: `Hello, ${decoded.username}`,
-            secret: `here is your authorized data, your lucky number is ${luckyNumber}`,
-        });
-    } catch (error) {
-        throw new CustomAPIError("Not Authorized to access this route", 401);
-    }
-
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`,
+        secret: `here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
 };
 
 module.exports = { login, dashboard };
